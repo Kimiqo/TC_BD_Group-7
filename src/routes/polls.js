3632aa5640ea6a34c8f0268f7c204789aa44e268
ndibe ebube <Ndibe-E>
2025-05-13 18:40:22 +0100

    The Routes

--- src/routes/polls.js
+++ src/routes/polls.js
@@ -7,11 +7,22 @@ const router = express.Router();
 //All functions are yet to be created
 
 // Create a poll - createPoll function
+router.post(
+  '/',
+  [
+    check('question').notEmpty().trim(),
+    check('options').isArray({ min: 2 }).custom(options => options.every(opt => typeof opt.text === 'string' && opt.text.trim())),
+  ],
+  createPoll
+);
 
 // Get all polls - getPolls function
+router.get('/', getPolls);
 
 // Get a single poll - getPoll function
+router.get('/:id', getPoll);
 
 // Vote on a poll - vote function
+router.post('/:id/vote', [check('optionId').notEmpty()], vote);
 
 export default router;
